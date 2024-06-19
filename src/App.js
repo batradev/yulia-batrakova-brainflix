3d3081b364e7a681eae313bbbca4473e2f6756b2
Yuliya Batrakova <batrakova.yulia@gmail.com>
2024-06-19 12:00:06 -0400

    Added api functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@
 import "./App.scss";
 import "./styles/main.scss";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import allVideos from "./data/videos.json";
+// import allVideos from "./data/videos.json";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from "./components/Header/Header";
 
 import UploadPage from "./pages/UploadPage/UploadPage";
 import VideoPlayerPage from './pages/VideoPlayerPage/VideoPlayerPage';
 
 function App() {
+  
+    const [videos, setVideos] = useState([]);
+  
+    useEffect(() => {
+      const fetchVideos = async () => {
+        try {
+          const response = await axios.get('https://unit-3-project-api-0a5620414506.herokuapp.com/videos?api_key=cbdf096d-46fe-4e06-8496-641c06f1cedf');
+          setVideos(response.data);
+        } catch (error) {
+          console.error('Error fetching videos:', error);
+        }
+      };
+      fetchVideos();
+    }, []);
   
   return (
     <div className="App">
@@ -19,7 +35,7 @@ function App() {
             path="/"
             element={
               <VideoPlayerPage
-                videos={allVideos}
+                videos={videos}
               />
             }
           />
@@ -28,7 +44,7 @@ function App() {
             path="/video/:videoId"
             element={
               <VideoPlayerPage
-                videos={allVideos}
+                videos={videos}
               />
             }
           />
